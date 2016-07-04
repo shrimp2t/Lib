55ab568dc8c055fff6264e8b9e41270260279e04
TruongSa <shrimp2t@gmail.com>
2016-07-04 08:39:21 +0700

    Update wp-screen-info-debug.php

--- wp-screen-info-debug.php
+++ wp-screen-info-debug.php
@@ -1,59 +1,60 @@
-/**
- * Plugin Name:       Admin Screen Info
- * Plugin URI:        http:shrimp2t.com/
- * Description:       Advance Ajax WordPress Login & Register Form.
- * Version:           1.0.0
- * Author:            SmoothThemes
- * Author URI:        http:shrimp2t.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       wpu
- * Domain Path:       /languages
- */
+<?php
 
+/**
+* Plugin Name:       Admin Screen Info
+* Plugin URI:        http:shrimp2t.com/
+* Description:       Advance Ajax WordPress Login & Register Form.
+* Version:           1.0.0
+* Author:            SmoothThemes
+* Author URI:        http:shrimp2t.com/
+* License:           GPL-2.0+
+* License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+* Text Domain:       wpu
+* Domain Path:       /languages
+*/
 
 
 
 // ==================for debug===============================
 if(!function_exists('s_help_screen_help')){
-    add_action( 'contextual_help', 's_help_screen_help', 10, 3 );
-    function s_help_screen_help( $contextual_help, $screen_id, $screen ) {
-        // The add_help_tab function for screen was introduced in WordPress 3.3.
-        if ( ! method_exists( $screen, 'add_help_tab' ) )
-            return $contextual_help;
-        global $hook_suffix;
-        // List screen properties
-        $variables = '<ul style="width:50%;float:left;"> <strong>Screen variables </strong>'
-            . sprintf( '<li> Screen id : %s</li>', $screen_id )
-            . sprintf( '<li> Screen base : %s</li>', $screen->base )
-            . sprintf( '<li>Parent base : %s</li>', $screen->parent_base )
-            . sprintf( '<li> Parent file : %s</li>', $screen->parent_file )
-            . sprintf( '<li> Hook suffix : %s</li>', $hook_suffix )
-            . '</ul>';
-        // Append global $hook_suffix to the hook stems
-        $hooks = array(
-            "load-$hook_suffix",
-            "admin_print_styles-$hook_suffix",
-            "admin_print_scripts-$hook_suffix",
-            "admin_head-$hook_suffix",
-            "admin_footer-$hook_suffix"
-        );
-        // If add_meta_boxes or add_meta_boxes_{screen_id} is used, list these too
-        if ( did_action( 'add_meta_boxes_' . $screen_id ) )
-            $hooks[] = 'add_meta_boxes_' . $screen_id;
-        if ( did_action( 'add_meta_boxes' ) )
-            $hooks[] = 'add_meta_boxes';
-        // Get List HTML for the hooks
-        $hooks = '<ul style="width:50%;float:left;"> <strong>Hooks </strong> <li>' . implode( '</li><li>', $hooks ) . '</li></ul>';
-        // Combine $variables list with $hooks list.
-        $help_content = $variables . $hooks;
-        // Add help panel
-        $screen->add_help_tab( array(
-            'id'      => 'wptuts-screen-help',
-            'title'   => 'Screen Information',
-            'content' => $help_content,
-        ));
-        return $contextual_help;
-    }
+add_action( 'contextual_help', 's_help_screen_help', 10, 3 );
+function s_help_screen_help( $contextual_help, $screen_id, $screen ) {
+// The add_help_tab function for screen was introduced in WordPress 3.3.
+if ( ! method_exists( $screen, 'add_help_tab' ) )
+return $contextual_help;
+global $hook_suffix;
+// List screen properties
+$variables = '<ul style="width:50%;float:left;"> <strong>Screen variables </strong>'
+    . sprintf( '<li> Screen id : %s</li>', $screen_id )
+    . sprintf( '<li> Screen base : %s</li>', $screen->base )
+    . sprintf( '<li>Parent base : %s</li>', $screen->parent_base )
+    . sprintf( '<li> Parent file : %s</li>', $screen->parent_file )
+    . sprintf( '<li> Hook suffix : %s</li>', $hook_suffix )
+    . '</ul>';
+// Append global $hook_suffix to the hook stems
+$hooks = array(
+"load-$hook_suffix",
+"admin_print_styles-$hook_suffix",
+"admin_print_scripts-$hook_suffix",
+"admin_head-$hook_suffix",
+"admin_footer-$hook_suffix"
+);
+// If add_meta_boxes or add_meta_boxes_{screen_id} is used, list these too
+if ( did_action( 'add_meta_boxes_' . $screen_id ) )
+$hooks[] = 'add_meta_boxes_' . $screen_id;
+if ( did_action( 'add_meta_boxes' ) )
+$hooks[] = 'add_meta_boxes';
+// Get List HTML for the hooks
+$hooks = '<ul style="width:50%;float:left;"> <strong>Hooks </strong> <li>' . implode( '</li><li>', $hooks ) . '</li></ul>';
+// Combine $variables list with $hooks list.
+$help_content = $variables . $hooks;
+// Add help panel
+$screen->add_help_tab( array(
+'id'      => 'wptuts-screen-help',
+'title'   => 'Screen Information',
+'content' => $help_content,
+));
+return $contextual_help;
+}
 }
 /// ------------------------------
